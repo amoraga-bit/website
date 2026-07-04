3999556b103d6695b775feaee60b9095619a19b8
Amílcar Moraga <amilcar.samael.moraga@clases.edu.sv>
2026-07-03 18:44:41 -0600

    Usando section en lugar de fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 const App = () => {
 return (
-  <React.Fragment>  
+  <section>  
     <h1>Mi primer componente de react</h1>
     <h2>Mi sitio web con react</h2>
-  </React.Fragment>
+  </section>
 )
 
 }
