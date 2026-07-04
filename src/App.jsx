3aff9440b159a4b81bdff80237c8ce478e9e2c37
Amílcar Moraga <amilcar.samael.moraga@clases.edu.sv>
2026-07-03 18:45:30 -0600

    Eliminando section y usando una forma similar al fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 const App = () => {
 return (
-  <section>  
+  <>  
     <h1>Mi primer componente de react</h1>
     <h2>Mi sitio web con react</h2>
-  </section>
+  </>
 )
 
 }
