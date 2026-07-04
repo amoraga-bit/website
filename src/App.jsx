e4c6191d1849d517295ec9a63298c0d3031b4895
Amílcar Moraga <amilcar.samael.moraga@clases.edu.sv>
2026-07-03 18:41:28 -0600

    feat: add SVG assets for React and Vite, set up main entry point and Vite configuration
    - Added react.svg and vite.svg to the assets directory.
    - Created main.jsx as the entry point for the React application.
    - Configured Vite with the necessary React plugin in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const App = () => {
+return (
+  <React.Fragment>  
+    <h1>Mi primer componente de react</h1>
+    <h2>Mi sitio web con react</h2>
+  </React.Fragment>
+)
+
+}
+export default App;
